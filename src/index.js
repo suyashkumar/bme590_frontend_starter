148816ae6b2387ba9a00b1254399ab934a030d3a
Suyash Kumar <suyashkumar2003@gmail.com>
2017-12-13 13:26:16 -0500

    Remove registerServiceWorker to allow for non-SSL deploys for students in class

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root')); 
